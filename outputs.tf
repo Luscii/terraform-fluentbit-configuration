fc1ad8d69f79af17a785959605764f7f553f5338
Boite <boite@luscii.com>
2026-01-20 15:06:50 +0100

    docs: add comprehensive terraform outputs documentation with examples

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "log_config_parsers" {
+  description = "Configuration details for the parser to be used in terraform module: Luscii/terraform-aws-ecs-fargate-datadog-container-definitions"
+  value       = ""
+}
+
+output "log_config_filters" {
+  description = "Configuration details for the filters to be used in terraform module: Luscii/terraform-aws-ecs-fargate-datadog-container-definitions"
+  value       = ""
+}
